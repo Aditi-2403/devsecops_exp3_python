07a17989cd992acf95715063b58adf7d54a6debc
Aditi-2403 <goeladiti24@gmail.com>
2026-08-21 15:24:56 +0530

    Add admin check function and update main logic

--- file.py
+++ file.py
@@ -6,6 +6,15 @@ def add_numbers(a, b):
     return a + b
 
 
+def check_admin(password):
+    admin_password = "Admin@12345"
+
+    if password == admin_password:
+        return "Access granted"
+    return "Access denied"
+
+
 if __name__ == "__main__":
     print(greet("Student"))
     print("Sum:", add_numbers(10, 20))
+    print(check_admin("Admin@12345"))
